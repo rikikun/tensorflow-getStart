f8e4aab1c7496bc98fb3b39cddcbb33e31087256
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2017-10-28 13:29:21 +0700

    Create linear model with optimize for find the best W and b to minimize loss function

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 """This module does blah blah."""
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
-NODE1 = tf.constant(3.0, dtype=tf.float32)
-NODE2 = tf.constant(4.0)  # also tf.float32 implicitly
-print(NODE1, NODE2)
+session = tf.Session()
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+init = tf.global_variables_initializer()
+session.run(init)
+
+y = tf.placeholder(tf.float32)
+loss = tf.reduce_sum(tf.square(linear_model - y))
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+for i in range(10000):
+    session.run(train, {x: [1, 2, 3, 4], y: [5, 9, 13, 17]})
+print session.run([W, b])
+
+print session.run(loss, {x: [1, 2, 3, 4], y: [5, 9, 13, 17]})
+
+file_writer = tf.summary.FileWriter(
+    '/Users/Thanakornthanprasit/ml/log-graph', session.graph)
