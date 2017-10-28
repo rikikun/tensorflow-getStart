fac815b3507d758600c8d8a06ee0183a03c0628e
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2017-10-28 13:31:33 +0700

    Refactor code

--- main.py
+++ main.py
@@ -16,11 +16,15 @@ loss = tf.reduce_sum(tf.square(linear_model - y))
 optimizer = tf.train.GradientDescentOptimizer(0.01)
 train = optimizer.minimize(loss)
 
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
+
 for i in range(10000):
-    session.run(train, {x: [1, 2, 3, 4], y: [5, 9, 13, 17]})
+    session.run(train, {x: x_train, y: y_train})
 print session.run([W, b])
 
-print session.run(loss, {x: [1, 2, 3, 4], y: [5, 9, 13, 17]})
+curr_W, curr_b, curr_loss = session.run([W, b, loss], {x: x_train, y: y_train})
+print("W: %s b: %s loss: %s" % (curr_W, curr_b, curr_loss))
 
 file_writer = tf.summary.FileWriter(
     '/Users/Thanakornthanprasit/ml/log-graph', session.graph)
